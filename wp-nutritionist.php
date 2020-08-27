c3b25317d91adf6bc28ec9cd17696c51b0d9591b
SimoneAntonicchio <simone.antonicchio@gmail.com>
2020-08-27 18:07:20 +0200

    plugin first impl

---
+++ wp-nutritionist.php
@@ -0,0 +1,10 @@
+/**
+ * Plugin Name:       WP-Nutritionist
+ * Plugin URI:        https://github.com/simoneantonicchio/wp-nutritionist
+ * Description:       Nutritionist plugin 
+ * Version:           0.0.1
+ * Author:            Simone Filippo Antonicchio
+ * Author URI:        https://github.com/simoneantonicchio
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ */
